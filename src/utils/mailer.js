87dc1f474564b1a9981dd77a1d10826a052ccae0
bharathi9989 <er.velubharathi.s@gmail.com>
2025-10-28 00:49:52 +0530

    pending

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,9 +1,36 @@
-// import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
-// /**
-//  * @desc Create and configure a Nodemailer transporter
-//  */
+/**
+ * @desc Create and configure a Nodemailer transporter
+ */
 
-// const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
-// })
+/**
+ * @desc Send email
+ * @param {String} to - recipient email
+ * @param {String} subject - email subject
+ * @param {String} html - email content in HTML
+ */
+
+export const sendEmail = async (to, subject, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `Auction Platform <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log(`Mail sent to ${to}:${info.messageId}`);
+  } catch (err) {
+    console.log(`❌Error sending email`);
+  }
+};
