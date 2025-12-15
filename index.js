14b6f46ead4ee3571afdc28ac9eed342e519cf8c
bharathi9989 <er.velubharathi.s@gmail.com>
2025-12-15 20:28:37 +0530

    Final deploy

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ app.use(helmet());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_ORIGIN || "http://localhost:5173",
+    origin:
+      process.env.FRONTEND_ORIGIN ||
+      "http://localhost:5173" ||
+      "https://auctionsplatform.netlify.app/",
     credentials: true,
   })
 );
