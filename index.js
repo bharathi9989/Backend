7e55b0cbf598a453d71c38698d27b4ed22cad0c4
bharathi9989 <er.velubharathi.s@gmail.com>
2025-12-16 11:06:03 +0530

    fixed

--- index.js
+++ index.js
@@ -23,15 +23,17 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
+import cors from "cors";
+
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://auctionsplatform.netlify.app/",
+  "https://auctionsplatform.netlify.app",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // allow Postman, curl
+      if (!origin) return callback(null, true);
 
       if (allowedOrigins.includes(origin)) {
         callback(null, true);
